7bab71a75874b15918e8206b16a2cbfc2f49a990
dev <dev@sattelite-dev-junior.(none)>
2012-05-24 15:15:49 +0300

    Added example for multiline write

--- examples/multiline_write.php
+++ examples/multiline_write.php
@@ -1,1 +1,28 @@
- 
+<?php
+    require_once('../image.php'); // Load image class
+    
+    $a = new Image(); // Create new Image object
+    
+    $small_string = 'Hello world!';   
+    $large_string = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam vestibulum velit sodales turpis feugiat dignissim pharetra dui consectetur. Aenean nec ante at enim sollicitudin ornare vel porta metus. Fusce lorem turpis, tristique nec consectetur vel, sodales cursus sapien. Curabitur in laoreet ante. ';
+
+    // simple write small text on the image
+        $a->write(100,200,'times.ttf',120,null,'#FFFFFF',$small_string);
+
+    // write large text to the some area on the image with automaticaly word-wrap
+    // and font size decreasing to fit all the text in specified box
+        $config = array(
+        'font' => 'times.ttf',
+                'startFontSize' => 150, // If text wouldn't fit the $boxHeight, then font size will be decreased
+                'stepFontSize' => 10,   // step for font size decreasing
+                'angle' => null,
+                'color' => '#FFFFFF',
+                'lineSpacing' => null,  // if not definedm then will be startFontSize/10
+                'padding' => '20 10',   // almost in css format :), acceptable two or one parameters
+        );
+        $boxWidth = null; // will be fetched from the $a->width
+        $boxHeight = 300;
+            $this->image->writeMultiline('center', 240, $boxWidth, $boxHeight, $large_string, $config);
+    
+    $a->show(); // Show image
+?>
